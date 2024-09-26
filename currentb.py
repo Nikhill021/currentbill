106c7c80d4a1330760fa7296e5827e26ebf3e7e7
Nikhill021 <nikhluuu@gmail.com>
2024-09-26 14:39:39 +0530

    Update currentb.py

--- currentb.py
+++ currentb.py
@@ -1,7 +1,7 @@
 def generate_bill(units):
     if units <= 200:
         bill_amount = 0
-        bill_status = "Free (Scheme)"
+        bill_status = "gruha jyothi sceme"
     else:
         bill_amount = (units - 200) * 5
         bill_status = "Charged"
